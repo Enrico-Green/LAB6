880ff3f66e60cc66aefe39f8535b82b33aacd2d8
enrico-green <enrico_green@Hotmail.com>
2019-03-11 15:48:00 -0700

    first try one large object

---
+++ js/app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+//display individual store data for Salmon Cookie Shops
+//data stored in a list format on the website
+//use object oriented programming 
+//use object literals one for each store model; no constructors
+//use template literals in your JS logic to render list on sales page
+
+
+/* KEYS
+location - loc
+min cust - min
+max custer -max
+avg cookies per sale -avg
+*/
+
+/* 
+1st and pike location, min cust 23, max custer 65, avg cookikes per sale 6.3
+
+seatac airport location, min cust 3, max cust 24, avg cookikes per sale 1.2 
+
+seattle center , min cust 11, max custer 38, avg cookikes per sale 3.7
+
+capitol hill location, min cust 20, max custer 3, avg cookikes per sale 2.3
+
+Alki location, min cust 2, max custer 16, avg cookikes per sale 4.6
+*/
+
+var cookieShops = {
+    loc: 'pike', 'seatac', 'seacent', 'caphill', 'alki', 
+    min: '23', '3', '11', '20', '2', 
+    max: '65', '24', '38', '3', '16',
+    avg: '6.3', '1.2', '3.7', '2.3', '4.6',
+
+}
+//output number of cookies per hour to make
