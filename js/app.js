1b2ef0e7d15a1c149e1efe2999357146c6facc9e
enrico-green <enrico_green@Hotmail.com>
2019-03-11 17:56:53 -0700

    app.js 1st test pike only

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ var pike = {
     max: '65',
     avg: '6.3', 
     // number of cookies per hour to make, when given the randomly select number of customers per hour
-    cookiesPerHour:[],
+    cookiesPerHourArray:[],
 };
 //seatac airport location, min cust 3, max cust 24, avg cookies per sale 1.2 
 var seatac = {
@@ -40,7 +40,7 @@ var seatac = {
     max: '24', 
     avg: '1.2', 
     // number of cookies per hour to make, when given the randomly select number of customers per hour
-    cookiesPerHour:[],
+    cookiesPerHourArray:[],
 };
 //seattle center , min cust 11, max custer 38, avg cookies per sale 3.7
 var seacen = {
@@ -49,7 +49,7 @@ var seacen = {
     max: '38', 
     avg: '3.7', 
     // number of cookies per hour to make, when given the randomly select number of customers per hour
-    cookiesPerHour:[],
+    cookiesPerHourArray:[],
 };
 //capitol hill location, min cust 20, max custer 3, avg cookies per sale 2.3
 var caphill = {
@@ -58,7 +58,7 @@ var caphill = {
     max: '38', 
     avg: '2.3', 
     // number of cookies per hour to make, when given the randomly select number of customers per hour
-    cookiesPerHour:[],
+    cookiesPerHourArray:[],
 };
 //Alki location, min cust 2, max custer 16, avg cookies per sale 4.6
 var alki = {
@@ -67,7 +67,7 @@ var alki = {
     max: '16',
     avg: '4.6',
     // number of cookies per hour to make, when given the randomly select number of customers per hour
-    cookiesPerHour:[],
+    cookiesPerHourArray:[],
 };
 
 
@@ -85,4 +85,24 @@ cookieList.textContent = 'cookies to make per hour';
 
 pikeList.appendChild(cookieList);
 
+for (var i = 0; i < this.cookiesPerHour.length; i++) {
+    var cookiesPikeList = document.createElement ('li');
+    var pikeCookies = this.cookiesPerHourArray[i];
+    cookiesPikeList.textContent = 'If ${i + 1} , customers per Hour: ${customersHour} ';
+
+    pikeList.appendChild(new_Li)
+} 
+
+}  ;
+
+pike.calculateCookiesPerHour = function () {
+for(var i = 0; i < 7; i++) {
+ var randomCustomersPerHour = getRandomIntCustomers(this.min, this.max)
+ var cookiesNeededForCustomer = randomCustomersPerHour * this.avg;
+ this.cookiesPerHourArray.push(cookiesNeededForCustomer)
 }
+
+}
+
+pike.calculateCookiesPerHour();
+pike.renderToPage();
