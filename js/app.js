176a4ba4763bd4575814da5c666106ce482640b9
enrico-green <enrico_green@Hotmail.com>
2019-03-12 22:24:13 -0700

    initial unfinished

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+'use trict'
+
+// webpage is going to display store data for salmon cookie shops
+//uses object oriented programming to build this site
+
+//represent store data in a list
+// use of object literals - no constructors
+//use template liters in your JS logic
+//render stores as lists on the sales page 
